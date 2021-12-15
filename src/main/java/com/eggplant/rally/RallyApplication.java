f046b49aed9857e0250b89903fa3a7c8077a11a6
SUHWAN KIM <oink12268@gmail.com>
2021-12-15 22:18:26 +0900

    vue login 화면 구현

--- src/main/java/com/eggplant/rally/RallyApplication.java
+++ src/main/java/com/eggplant/rally/RallyApplication.java
@@ -1,13 +1,20 @@
 package com.eggplant.rally;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log4j2
 @SpringBootApplication
 public class RallyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RallyApplication.class, args);
+        log.info("*----------------*");
+        log.info("|                |");
+        log.info("|   SUCCESS !!!  |");
+        log.info("|                |");
+        log.info("*----------------*");
     }
 
 }
